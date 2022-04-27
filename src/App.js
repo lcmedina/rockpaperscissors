51963af866965d63680f2b9a99ee0655738621b6
Laura Medina <lcmedina390@gmail.com>
2022-04-27 12:06:08 -0500

    triangle set up for web

--- src/App.js
+++ src/App.js
@@ -14,6 +14,23 @@ function App() {
           <h1>12</h1>
        </div>
      </div>
+     <div className="game">
+        <div className="paperbg">
+          <div className="paper">
+            <img src="./images/icon-paper.svg" alt="paper" />
+          </div>
+        </div>
+        <div className="scissorsbg">
+          <div className="scissors">
+            <img src="./images/icon-scissors.svg" alt="scissors" />
+          </div>
+        </div>
+        <div className="rockbg">
+          <div className="rock">
+            <img src="./images/icon-rock.svg" alt="rock" />
+          </div>
+        </div>
+     </div>
     </div>
   );
 }
